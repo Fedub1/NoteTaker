19a6855e4ed6bf2df6cc18046802701138363d59
Felicia Wright <fedub1@yahoo.com>
2020-12-12 21:37:01 -0500

    updates routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,53 +1,42 @@
 // Require/import the HTTP module
 
 var notes = require("../db/db.json");
-// var index = require("./public/index");
+
+module.exports = function (app) {
 
 
-// const fs = require("fs");
+  // Establish Routes
 
-
-module.exports = function(app) {
-
-
-// Establish Routes
-
-app.get("/api/notes", function(req, res) {
-    res.json(db);
+  app.get("/api/notes", function (req, res) {
+    res.json(notes);
   });
 
-  // app.get("/api/notes/:id", function (req, res) {
-  //     res.json(req.params.id);
-  //       });
-        
-app.post("/api/notes", function(req, res) {
-  const newNote = req.body;
-      if (notes.length === 0) {
+ 
+  app.post("/api/notes", function (req, res) {
+    const newNote = req.body;
+    if (notes.length === 0) {
       notes.push(req.body);
       res.json(true);
     } else {
-    newNote.id = (notes[notes.length-1].id + 1)
-    notes.push(req.body);
-    res.json(true);
-  }
-{
-    notes.push(req.body);
-    res.json(false);
-  }
-});
-},
-app.delete("/api/notes/:id", function (req, res) {
-  res.json(req.params.id);
-  });
-
-    db.Notes.destroy({
-      where: {
-        id: req.params.id
-      }
-    })
-      .then(function(notes) {
-        res.json(notes);
-      });
+      newNote.id = (notes[notes.length - 1].id + 1);
+      notes.push(req.body);
+      res.json(true);
+    }
   
-     
-
+  });
+  app.delete('/api/notes/:id', function (req, res) {
+    res.json(req.params.id);
+var id = req.params.id.toString();
+   var i;
+    for (i=0; i < notes.length; i++) {
+      if(notes[i].id == id){
+        res.send(notes[i]);
+   
+    notes.splice(i, 1);
+        console.log("Note deleted");
+        break;
+      }}
+  }
+  
+);
+    };
